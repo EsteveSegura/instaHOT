a961d4ff73c9a1d2a528624ddbcf972f893e0244
Esteve Segura <estevesegura.gir@gmail.com>
2019-11-08 21:01:03 +0100

    recover data

---
+++ src/dataRecovery/convertData.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function delay(s){
+     return new Promise((resolve,reject) =>{
+          setTimeout(() => {
+               resolve("OK")
+          }, 1000 * s);
+     })
+}
+
+async function getNewData(){
+     return new Promise ((resolve , reject) =>{
+          let data = fs.readFile('./oldDataBase.json', async(err, f) =>{
+               let allData = f.toString().split('\n');
+               let instaUser = allData.map((line,index) =>{
+                    let splitOne = line.split('"user":"')[1]
+                    let splitTwo = splitOne.split('"')[0]
+                    return splitTwo
+               })
+               resolve(instaUser)
+          });
+     })
+}
+(async () => {
+     let data = await getNewData()
+     console.log(data)
+})(); 
+
+module.exports = { getNewData } 
