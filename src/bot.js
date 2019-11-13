b0cd19d5a9a3e5ee41e976047aa576997a929979
Esteve Segura <estevesegura.gir@gmail.com>
2019-11-13 18:24:46 +0100

    post url if is finded

--- src/bot.js
+++ src/bot.js
@@ -6,21 +6,21 @@ const fs = require('fs')
 const token = process.env.TELEGRAM_TOKEN
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 
 bot.on('message', (msg) => {
      const chatId = msg.chat.id;
      console.log(chatId)
 
-     if(msg.from.id != 3119961521){
+     if (msg.from.id != 3119961521) {
           bot.deleteMessage(msg.chat.id, msg.message_id)
      }
 
      //bot.sendMessage(chatId, 'Received your message');
 });
 
-function readFolder(){
+function readFolder() {
      folder = []
      fs.readdirSync('./tmp').forEach(file => {
           folder.push(file)
@@ -31,19 +31,18 @@ function readFolder(){
 
 setInterval(() => {
      let files = readFolder()
-     if(files.length != 0){
+     if (files.length != 0) {
           let urlBase = "https://instagram.com/"
-          try {
-               let urlUser = files[0].split('_ig_')[0]
-               console.log(`full URL : ${urlBase}${urlUser}`)
-          } catch (error) {
-               
+          let urlUser = files[0].split('_ig_')[0]
+          if(files[0].includes('_ig_')){
+               bot.sendPhoto("-1001325433576", `./tmp/${files[0]}`, { caption: `profile : ${urlBase}${urlUser}` })
+          }else{
+               bot.sendPhoto("-1001325433576", `./tmp/${files[0]}`)
           }
-          bot.sendPhoto("-1001325433576",`./tmp/${files[0]}`)
           console.log('sending pictures')
           setTimeout(() => {
                donwload.deleteImage(`./tmp/${files[0]}`)
           }, 1000);
      }
-}, 60000*5);
+}, 60000 * 5);
 
